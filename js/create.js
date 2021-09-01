e3a3095e35c03689646c71e411c6318e3ea2a23e
Adventeen <tushar.lifetg@gmail.com>
2021-09-01 19:28:48 +0530

    Added fucntionality in quiz.html

--- js/create.js
+++ js/create.js
@@ -5,53 +5,46 @@ class Question {
       this.correct = correct;
     }
   }
-  
+
   let ques_arr = []; // array in which the quiz will be stored
-  
+
   // event listerner for add question button
   document.getElementById('add').addEventListener('submit', (e) => {
     e.preventDefault();
     var ques = document.getElementById('ques').value;
-    document.getElementById('ques').value = '';
     var correct = -1;
     let opts_value = [];
-  
+
     var opts = document.getElementsByClassName('opt');
     var checkbox = document.getElementsByClassName('answer');
-  
+
     for (var i = 0; i < 4; i++) {
       opts_value[i] = opts[i].value;
-      opts[i].value = '';
       if(checkbox[i].checked) {
         correct = i;
         checkbox[i].checked = false;
       }
     }
-    if(correct===-1){
+    if(correct === -1) {
         alert("Choose the correct option.");
     }
-    else{
+    else {
         const question = new Question(ques, opts_value, correct);
-    ques_arr.push(question);
-    document.getElementById('numQ').textContent = ques_arr.length;
-
+        ques_arr.push(question);
+        document.getElementById('numQ').textContent = ques_arr.length;
+        for (var i = 0; i < 4; i++) {
+          opts[i].value = '';
+        }
+        document.getElementById('ques').value = '';
     }
-    
+
     // console.log(question);
   })
-  
+
   // event listerner for adding the quiz to local storage
   document.getElementById('SubmitB').addEventListener('click', (e) => {
     e.preventDefault();
     localStorage.setItem("quiz", JSON.stringify(ques_arr));
     document.getElementById('numQ').textContent = 0;
     console.log(ques_arr);
-    // validateForm();
   })
-  function validateForm() {
-    let x = document.forms["myForm"]["answer"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
--- js/quiz.js
+++ js/quiz.js
@@ -1,22 +1,7 @@
 var ques_arr = JSON.parse(localStorage.getItem("quiz"));
 correct = -1;
 
-// Display play now when the page is loaded
-document.addEventListener("DOMContentLoaded", () => {
-   document.getElementById('main').style.display = 'None';
-});
-
-document.getElementById('play').addEventListener('click', () => {
-  document.getElementById('main').style.display = 'block';
-  document.getElementById('playnow').style.display = 'None';
-  game();
-});
-
 function game() {
-  // var timeleft =  Math.floor(document.getElementById('timer').textContent);
-  // timeleft -= 1;
-  // document.getElementById('timer').textContent = timeleft;
-
   var quesnum =  Math.floor(document.getElementById('quesnum').textContent);
 
   document.getElementById('ques-title').textContent = ques_arr[quesnum - 1]['ques']
@@ -26,11 +11,89 @@ function game() {
     opts[i].textContent = ques_arr[quesnum - 1]['opts'][i];
   }
   correct = ques_arr[quesnum - 1]['correct'];
+  document.getElementById('timer').textContent = 10;
 }
 
-document.getElementById('check').addEventListener('click', () => {
-  options = document.getElementsByClassName('opt');
+function timer() {
+  if (document.getElementById('check').textContent == "Check") {
+    var timeleft =  Math.floor(document.getElementById('timer').textContent);
+    timeleft -= 1;
+    document.getElementById('timer').textContent = timeleft;
+    if (timeleft == 0) {
+      document.getElementById('check').textContent = 'Next';
+      document.getElementsByClassName('opt-div')[correct].style.border = "2px solid green";
+    }
+  }
+}
 
+
+// Display play now when the page is loaded
+document.addEventListener("DOMContentLoaded", () => {
+   document.getElementById('main').style.display = 'None';
+   document.getElementById('end').style.display = 'None';
+});
+
+// Start the game
+document.getElementById('play').addEventListener('click', () => {
+  document.getElementById('main').style.display = 'block';
+  document.getElementById('playnow').style.display = 'None';
+  setInterval(timer, 1000);
+  game();
+});
+
+
+document.getElementById('check').addEventListener('click', (e) => {
+  options = document.getElementsByClassName('opt-radio');
+  var marked = -1;
+  // Checking if the correct ans is marked
+  if (e.target.textContent == 'Check') {
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].checked) {
+        marked = i;
+      }
+    }
+    if (marked == -1) {
+      alert("Pls choose one option.");
+    }
+    else {
+      if (Math.floor(document.getElementById('quesnum').textContent) == ques_arr.length) {
+        document.getElementById('check').textContent = 'End'; //If the question is last
+      } else {
+        document.getElementById('check').textContent = 'Next';
+      }
+      if(marked === correct) {
+        var score =  Math.floor(document.getElementById('score').textContent);
+        score += 10;
+        document.getElementById('score').textContent = score;
+        document.getElementsByClassName('opt-div')[correct].style.border = "2px solid green";
+      }
+      else {
+        document.getElementsByClassName('opt-div')[correct].style.border = "2px solid green";
+        document.getElementsByClassName('opt-div')[marked].style.border = "2px solid red";
+      }
+    }
+  }
+  else if (e.target.textContent == 'Next') {
+    // Moving to next question
+    var quesnum =  Math.floor(document.getElementById('quesnum').textContent);
+    quesnum += 1;
+    document.getElementById('quesnum').textContent = quesnum;
+
+    optDiv = document.getElementsByClassName('opt-div');
+    for (var i = 0; i < 4; i++) {
+      options[i].checked = false;
+      optDiv[i].style.border = "0px";
+    }
+
+    document.getElementById('check').textContent = 'Check';
+    game();
+  }
+  else {
+    // Display the score card
+    var score =  Math.floor(document.getElementById('score').textContent);
+    document.getElementById('main').style.display = 'None';
+    document.getElementById('end').style.display = 'block';
+    document.getElementById('endscore').textContent = score;
   }
 
 });
